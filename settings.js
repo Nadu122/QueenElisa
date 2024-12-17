62376390163bfaeedc0ebeb75cb6074b75a3c5b1
Nadubot <Slnadumd2007@gmail.com>
2024-12-17 19:58:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==QShBnaMFkWmZmYLlVdiNEVXx0cTd1N2kWLtQzb1YEcURjQwRTVtQ0bfNHNjEFTTdkUFR2Ypwd=23365267749',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
